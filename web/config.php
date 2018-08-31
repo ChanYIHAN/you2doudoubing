85b56e387ed9ed535ecd57c7b353bbe03d11ed0a
ChanYIHAN <42235633+ChanYIHAN@users.noreply.github.com>
2018-08-31 15:46:28 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', '开心上网呀');
+define('TITLENAME', 'YIHANTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
